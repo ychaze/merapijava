c42c261adcbc3201b0847fd67ffd8fc358e71f21
Yannick CHAZE <yannick@wearecloud.com>
2008-12-03 14:25:55 +0100

    deuxieme commit

--- src/DataSource.java
+++ src/DataSource.java
@@ -13,14 +13,44 @@ public class DataSource implements Externalizable {
 	@Override
 	public void readExternal(ObjectInput arg0) throws IOException,
 			ClassNotFoundException {
-		
-
 	}
 
 	@Override
 	public void writeExternal(ObjectOutput arg0) throws IOException {
 		// TODO Auto-generated method stub
 
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
